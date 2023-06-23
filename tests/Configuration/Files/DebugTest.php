8ce3e9738ff7722155d85686f244805e48ece632
Javier <jpra110@gmail.com>
2023-06-23 03:15:39 -0300

    revert test fail

--- tests/Configuration/Files/DebugTest.php
+++ tests/Configuration/Files/DebugTest.php
@@ -56,7 +56,6 @@ class DebugTest extends TestCase
         $this->assertEquals(true, $aDbg);
         $this->assertEquals('DEBUG', $aLvl);
         $this->assertEquals('test.log', $aPat);
-        $this->assertEquals(false, true);
 
         //revert to original state
         $dto->setLog(false);
